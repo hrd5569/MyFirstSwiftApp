cfa590b08978451d864de72e113ff35827ab99a2
原田光 <haradahikari@HRDMacBook-Air.local>
2025-11-29 18:05:04 +0900

    Initial commit: MyFirstSwiftApp

---
+++ MyFirstSwiftApp/MyFirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstSwiftAppApp.swift
+//  MyFirstSwiftApp
+//
+//  Created by 原田光 on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
