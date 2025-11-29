c2d43dcbba79b5349d437558f50e4fcd5c7643d8
原田光 <haradahikari@HRDMacBook-Air.local>
2025-11-30 03:29:03 +0900

    カウンターとリセットボタン実装

--- MyFirstSwiftApp/ContentView.swift
+++ MyFirstSwiftApp/ContentView.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count : Int = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("カウンターアプリ")
+                .font(.title)
+            
+            Text("今の数: \(count)")
+                .font(.largeTitle)
+            
+            Button("1+する") {
+                count = count + 1
+            }
+            .font(.title2)
+            
+            Button("0にリセット") {
+                count = 0
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
